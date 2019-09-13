782c51e7e2db7c3a5fd1d34f4c9000f3cc8186d3
patarapolw <patarapolw@gmail.com>
2019-09-13 19:56:01 +0700

    fix ankiMustache

--- src/util.ts
+++ src/util.ts
@@ -1,20 +1,20 @@
 export function ankiMustache(s: string, d: Record<string, any> = {}, front: string = ""): string {
-  s = s.replace(/{{FrontSide}}/g, front.replace(/@html\n/g, ""))
+  s = s.replace(/\{\{FrontSide}}/g, front.replace(/@html\n/g, ""))
 
   for (const [k, v] of Object.entries(d)) {
       if (typeof v === "string") {
           s = s.replace(
-              new RegExp(`{{(\\S+:)?${escapeRegExp(k)}}}`, "g"),
+              new RegExp(`\\{\\{(\\S+:)?${escapeRegExp(k)}}}`, "g"),
               v.replace(/^@[^\n]+\n/gs, "")
           );
       }
   }
 
-  s = s.replace(/{{#(\S+)}}([^]*){{\1}}/gs, (m, p1, p2) => {
+  s = s.replace(/\{\{#(\S+)}}([^]*)\{\{\1}}/gs, (m, p1, p2) => {
       return Object.keys(d).includes(p1) ? p2 : "";
   });
 
-  s = s.replace(/{{[^}]+}}/g, "");
+  s = s.replace(/\{\{[^}]+}}/g, "");
 
   return s;
 }
